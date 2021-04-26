a8007e2890ad34c6359ed776ab9eb3294a96df4a
DuongHieu <duonghieu0712z@gmail.com>
2021-04-26 08:35:42 +0700

    Update project files.

--- src/window/Window.java
+++ src/window/Window.java
@@ -4,8 +4,6 @@ import java.awt.Font;
 import java.awt.Color;
 import java.awt.Insets;
 import java.awt.GridLayout;
-import java.awt.Dimension;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
